4926c8780c511b6055dee677da1d9fbf10834631
PhMueller <muller-phil@gmx.net>
2019-06-14 11:06:43 +0200

    Remove merge issue

--- examples/FMin.py
+++ examples/FMin.py
@@ -1,10 +1,10 @@
-"""
-Welcome to EasyOpt for HpBandSter optimization runs.
+﻿"""
+Welcome to FMin for HpBandSter optimization runs.
 
 You can easily optimize a function with the optimization tools from HpBandSter,
 like BOHB.
 
-The function easy_opt is callable from the commandline, as well as in your code.
+The function fmin is callable from the commandline, as well as in your code.
 
 To call it from command line, you must:
 
@@ -12,13 +12,10 @@ To call it from command line, you must:
 - provide the path to a configuration space definition in pcs or json file
     format
 - make sure to satisfy the conditions described in the docstring of
-    :method:: easy_opt
+    :method:: fmin
 
 """
 
-# TODO:
-# - Remove visualization at end of file (in main)
-# - Think of better way to pass function arguments via commandline
 
 import argparse
 from pathlib import Path
@@ -28,8 +25,9 @@ import hpbandster.core.result as hpres
 from hpbandster.optimizers import BOHB
 from hpbandster.core.worker import Worker
 
+from ConfigSpace.read_and_write import pcs_new, json
 
-class EasyOptWorker(Worker):
+class FMinWorker(Worker):
     """
     The worker is responsible for evaluating a single configuration on a
     single budget at a time.
@@ -49,7 +47,7 @@ class EasyOptWorker(Worker):
     """
 
     def __init__(self, func, func_args,  *args, **kwargs):
-        super(EasyOptWorker, self).__init__(*args, **kwargs)
+        super(FMinWorker, self).__init__(*args, **kwargs)
         self.func = func
         self.func_args = func_args
 
@@ -58,9 +56,9 @@ class EasyOptWorker(Worker):
                 'info': {'budget': budget}}
 
 
-def easy_opt(func, config_space, func_args=(),
-             eta=2, min_budget=2, max_budget=4, num_iterations=1,
-             num_workers=1, output_dir='.'):
+def fmin(func, config_space, func_args=(),
+          eta=2, min_budget=2, max_budget=4, num_iterations=1,
+          num_workers=1, output_dir='.'):
     """
     Starts a local BOHB optimization run for a function over a hyperparameter
     search space, which is referred to as configuration space.
@@ -94,13 +92,13 @@ def easy_opt(func, config_space, func_args=(),
     - Function arguments in right order:
         Function arguments, which are not hyperparameters and therefore not
         defined in the configuration space must be passed to the
-        ``easy_opt`` call in the order of occurrence in the function signature.
+        ``fmin`` call in the order of occurrence in the function signature.
         In the example below, for instance the training data, X and y, is a
         use case for this kind of function arguments.
 
     Example::
         import numpy as np
-        from EasyOpt import easy_opt
+        from FMin import fmin
         import ConfigSpace as CS
 
         # Create configuration space
@@ -119,7 +117,7 @@ def easy_opt(func, config_space, func_args=(),
         # The expected minimum is at w = 1.
         opt_func = lambda x, y, w, budget: np.mean((y[:int(budget)] - w*x[:int(budget)])**2)
 
-        inc_best, inc_best_cfg, result = easy_opt(opt_func,
+        inc_best, inc_best_cfg, result = fmin(opt_func,
                                                   cs, func_args=(X, y),
                                                   min_budget=3,
                                                   max_budget=len(X),
@@ -184,8 +182,10 @@ def easy_opt(func, config_space, func_args=(),
 
     """
     output_dir = Path(output_dir)
+    output_dir.mkdir(exist_ok=True)
+
     # Set up a local nameserver and start it
-    ns = hpns.NameServer(run_id='easy_opt',
+    ns = hpns.NameServer(run_id='fmin',
                          nic_name=None,
                          working_directory=output_dir)
     ns_host, ns_port = ns.start()
@@ -194,10 +194,10 @@ def easy_opt(func, config_space, func_args=(),
     # function arguments to each of them.
     workers = []
     for _ in range(num_workers):
-        worker = EasyOptWorker(func=func, func_args=func_args,
+        worker = FMinWorker(func=func, func_args=func_args,
                                nameserver=ns_host,
                                nameserver_port=ns_port,
-                               run_id='easy_opt')
+                               run_id='fmin')
         worker.run(background=True)
         workers.append(worker)
 
@@ -208,12 +208,12 @@ def easy_opt(func, config_space, func_args=(),
 
     # For hyperparameter importance analysis via CAVE we store the configuration
     # space definition to file.
-    with open(output_dir / 'configSpace.pcs', 'w') as f:
-        f.write(pcs_new.write(config_space))
+    with open(output_dir / 'configSpace.json', 'w') as f:
+        f.write(json.write(config_space))
 
     # Set up a master, which is book keeping and decides what to run next.
     opt = BOHB(configspace=config_space,
-               run_id='easy_opt',
+               run_id='fmin',
                min_budget=min_budget,
                max_budget=max_budget,
                eta=eta,
@@ -230,6 +230,11 @@ def easy_opt(func, config_space, func_args=(),
     opt.shutdown(shutdown_workers=True)
     ns.shutdown()
 
+    # Save to result object to file.
+    with open(output_dir / 'results.pkl', 'wb') as f:
+        import pickle
+        pickle.dump(result, f)
+
     # Return the optimal value and the responding configuration, as well as the
     # result object. The result object can be used in a second step for further
     # hyperparameter importance analysis with CAVE.
@@ -241,8 +246,50 @@ def easy_opt(func, config_space, func_args=(),
     return inc_value, inc_cfg, result
 
 
+def load_func(path_to_function_file):
+    """
+    Parse optimization function
+
+    Args:
+        path_to_function_file (str): Path to the file, in which the function
+            with the name 'opt_func' is
+
+    Returns:
+        optimize function
+    """
+    from importlib.machinery import SourceFileLoader
+    loader = SourceFileLoader('opt_func', path_to_function_file)
+    module = loader.load_module()
+    function = getattr(module, 'opt_func')
+    return function
+
+
+def load_configspace(path_to_cs_file):
+    """
+    Load configuration space definition
+    Args:
+        path_to_cs_file: Path to the file, in which the configuration space is
+            defined. Must be in format pcs or json
+
+    Returns:
+        ConfigSpace.configuration_space
+    """
+    if path_to_cs_file.endswith('.pcs'):
+        with open(path_to_cs_file, 'r') as f:
+            cfg = pcs_new.read(f)
+    elif path_to_cs_file.endswith('.json'):
+        with open(path_to_cs_file, 'r') as f:
+            cfg = json.read(f.read())
+    else:
+        raise ImportError('Configuration space definition not understood. File'
+                          ' must be in format pcs or json.')
+
+    return cfg
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Easy Optimization for '
+
+    parser = argparse.ArgumentParser(description='Easy Usage of '
                                                  'HpBandSter')
     parser.add_argument('--func',
                         help='Path to a py-file, containing a function to '
@@ -253,9 +300,9 @@ if __name__ == "__main__":
     parser.add_argument('--eta', help='ETA Parameter for HpBandSter',
                         type=float, default=2)
     parser.add_argument('--min_budget', help='Minimum budget for HpBandSter',
-                        type=float, default=2)
+                        type=float, default=1)
     parser.add_argument('--max_budget', help='Maximum budget for HpBandSter',
-                        type=float, default=4)
+                        type=float, default=1)
     parser.add_argument('--num_iterations',
                         help='Number iterations of HpBandSter',
                         type=int, default=1)
@@ -263,57 +310,16 @@ if __name__ == "__main__":
                         type=int, default=1)
     parser.add_argument('--output_dir', help='Output directory',
                         type=str, default='.')
-
     args = parser.parse_args()
 
-    # Parse optimization function
-    from importlib.machinery import SourceFileLoader
-    loader = SourceFileLoader('opt_func', args.func)
-    module = loader.load_module()
-    func = getattr(module, 'opt_func')
+    func = load_func(args.func)
+    config = load_configspace(args.config_space)
 
-    # Load configuration space definition
-    config = None
-    if args.config_space.endswith('.pcs'):
-        from ConfigSpace.read_and_write import pcs_new
-        with open(args.config_space, 'r') as f:
-            config = pcs_new.read(f)
-
-    elif args.config_space.endswith('.json'):
-        from ConfigSpace.read_and_write import json
-        with open(args.config_space, 'r') as f:
-            config = json.read(f.read())
-    else:
-        raise ImportError('Configuration space definition not understood. File'
-                          ' must be in format pcs or json.')
-
-    inc_value, inc_cfg, result = easy_opt(func=func, config_space=config, eta=args.eta,
+    inc_value, inc_cfg, result = fmin(func=func, config_space=config, eta=args.eta,
              min_budget=args.min_budget, max_budget=args.max_budget,
              num_iterations=args.num_iterations, num_workers=args.num_workers,
              output_dir=args.output_dir)
 
-    print('Found best value {} with the configuration {}'.format(inc_value,
+    print('Found best value {} with the configuration {}\n'.format(inc_value,
                                                                  inc_cfg))
-
-    # TODO THIS IS FOR DEMONSTRATION PURPOSE:
-    # Plot the results
-    id2config = result.get_id2config_mapping()
-    incumbent = result.get_incumbent_id()
-
-    traj = result.get_incumbent_trajectory()
-    budgets = [b for b in traj['budgets']]
-    values = [id2config[id]['config'] for id in traj['config_ids']]
-
-    import matplotlib.pyplot as plt
-    import numpy as np
-    np.random.seed(123)
-    X = np.random.uniform(-5, 5, 100)
-    y = np.random.normal(X, 1)
-    plt.scatter(X, y)
-    plt.xlim(-5, 5)
-    plt.ylim(-5, 5)
-    for i in range(len(values)):
-        plt.plot(X, values[i]['w'] * X,
-                 label="{}. W: {:.2f}".format(i + 1, values[i]['w']))
-    plt.legend(loc=1)
-    plt.show()
+    print('Result object is stored to {}'.format(args.output_dir))
