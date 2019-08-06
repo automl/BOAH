6a7ee65e09dc75faffb7d465dcf8088405fc4634
Joshua Marben <marbenj@informatik.uni-freiburg.de>
2019-08-06 19:39:26 +0200

    cluster

--- examples/icml_2018_experiments/scripts/cluster/job_pendulum.sh
+++ examples/icml_2018_experiments/scripts/cluster/job_pendulum.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#SBATCH -p queue-name # partition (queue)
+#SBATCH -p test_cpu-ivy # partition (queue)
 #SBATCH --mem 4000 # memory pool for all cores (4GB)
-#SBATCH -t 1-10:00 # time (D-HH:MM)
+#SBATCH -t 0-01:00 # time (D-HH:MM)
 #SBATCH -c 4 # number of cores
+#SBATCH -J pendulum_test # sets the job name. If not specified, the file name will be used as job name
 #SBATCH -o log/%x.%N.%j.out # STDOUT  (the folder log has to be created prior to running or this won't work)
 #SBATCH -e log/%x.%N.%j.err # STDERR  (the folder log has to be created prior to running or this won't work)
 #SBATCH --mail-type=END,FAIL # (recive mails about end and timeouts/crashes of your job)
@@ -14,14 +15,14 @@ echo "Started at $(date)";
 echo "Running job $SLURM_JOB_NAME using $SLURM_JOB_CPUS_PER_NODE cpus per node with given JID $SLURM_JOB_ID on queue $SLURM_JOB_PARTITION"; 
 echo "SLURM_ARRAY_TASK_ID: $SLURM_ARRAY_TASK_ID"; 
 
-source ~/BOHBsCAVE/.ve_BOHBsCAVE/bin/activate;
+source ~/virtual_envs/BOAH_pendulum/bin/activate;
 
 # Job to perform
 if [ $SLURM_ARRAY_TASK_ID -eq 0 ]
 then
-   python run_experiment.py --exp_name pendulum --run_id 43 --nic_name eth0 --no_worker --opt_method bohb --dest_dir opt_results/pendulum/bohb --min_budget 1 --n_workers 4
+   python run_experiment.py --exp_name pendulum --run_id 43 --nic_name eth0 --no_worker --opt_method bohb --dest_dir ../opt_results --min_budget 1 --n_workers 4
 else
-   python run_experiment.py --exp_name pendulum --run_id 43 --nic_name eth0 --worker --opt_method bohb --dest_dir opt_results/pendulum/bohb --min_budget 1 --n_workers 4
+   python run_experiment.py --exp_name pendulum --run_id 43 --nic_name eth0 --worker --opt_method bohb --dest_dir ../opt_results --min_budget 1 --n_workers 4
 fi
 
 # Print some Information about the end-time to STDOUT
